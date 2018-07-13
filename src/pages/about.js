d5fedeaa9628bed7de196b3064d8702a095658bd
Rafael Derkach <rafael_derkach@epam.com>
2018-07-13 13:04:42 +0300

    add img

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,20 @@
 import React, {Fragment} from 'react';
+import img1 from '../img/img1.jpg';
+import img2 from '../img/img2.jpg'
 
 const AboutPage = () => {
   return (
       <div>
          <h2>  About </h2>
-         
+         <section>
+           <div>
+<img src={img1} alt="" width='300'/>
+           </div>
+           <div>
+           <img src={img2} alt="" width='300'/>
+
+           </div>
+         </section>
           </div>
  
   )
